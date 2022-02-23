ebd3e58e03c5cdce6915157520c3fdc5e89f54c7
jcarmac3 <pokemonpower92@gmail.com>
2022-02-23 09:11:16 -0500

    Form fix and Dashboard work

--- app/client/src/pages/Dashboard.js
+++ app/client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Tab, Row, Col, Nav } from "react-bootstrap";
+import { Tab, Tabs } from "react-bootstrap";
 import ApplicantCard from "../components/ApplicantCard";
 import "../styles/pages/Dashboard.css";
 
@@ -11,35 +11,21 @@ function Dashboard() {
 	const admin = (
 		<div className="Dashboard-Admin">
 			<div className="Dashboard-heading">
-				<h1>This is the mock up of a dashboard.</h1>
+				<h1>This is the mock up of an admin dashboard.</h1>
 			</div>
-			<Tab.Container id="left-tabs-example" defaultActiveKey="first">
-				<Row>
-					<Col sm={3}>
-						<Nav variant="pills" className="flex-column">
-							<Nav.Item>
-								<Nav.Link eventKey="first">Pending</Nav.Link>
-							</Nav.Item>
-							<Nav.Item>
-								<Nav.Link eventKey="second">Completed</Nav.Link>
-							</Nav.Item>
-						</Nav>
-					</Col>
-					<Col sm={9}>
-						<Tab.Content className="Dashboard-applicantlist">
-							<Tab.Pane eventKey="first">
-								<h1>Pending Applicants</h1>
-								<ApplicantCard />
-								<ApplicantCard />
-								<ApplicantCard />
-							</Tab.Pane>
-							<Tab.Pane eventKey="second">
-								<h1>Completed Applicants</h1>
-							</Tab.Pane>
-						</Tab.Content>
-					</Col>
-				</Row>
-			</Tab.Container>
+			<Tabs
+				defaultActiveKey="pending"
+				id="uncontrolled-tab-example"
+				className="Dashboard-tabs"
+			>
+				<Tab eventKey="pending" title="Pending">
+					<ApplicantCard />
+					<ApplicantCard />
+					<ApplicantCard />
+				</Tab>
+				<Tab eventKey="flagged" title="Flagged"></Tab>
+				<Tab eventKey="completed" title="Completed"></Tab>
+			</Tabs>
 		</div>
 	);
 
