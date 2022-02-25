fedfcd21d67121918138c06e43d8818247eccd41
Logant08 <logantillman@ymail.com>
2022-02-25 15:55:15 -0500

    Create backend skeleton

---
+++ app/controllers/applicationController.js
@@ -0,0 +1,29 @@
+const Application = require("../models/application");
+
+createApplication = async (req, res) => {
+
+};
+
+getApplications = async (req, res) => {
+
+};
+
+getApplicationById = async (req, res) => {
+
+};
+
+updateApplication = async (req, res) => {
+
+};
+
+deleteApplication = async (req, res) => {
+
+};
+
+module.exports = {
+    createApplication,
+    getApplications,
+    getApplicationById,
+    updateApplication,
+    deleteApplication,
+};
