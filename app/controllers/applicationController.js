191e1e8429ea9cb3dc110c7f41b1b449a8c82144
Logant08 <logantillman@ymail.com>
2022-03-01 18:51:31 -0500

    Implement updateApplication

--- app/controllers/applicationController.js
+++ app/controllers/applicationController.js
@@ -23,7 +23,54 @@ getApplicationById = async (req, res) => {
 };
 
 updateApplication = async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
 
+    if (!id) {
+        res.status(400).json({
+            message: "No id given",
+        });
+        return;
+    }
+    if (!body) {
+        res.status(400).json({
+            message: "No body given",
+        });
+        return;
+    }
+
+    const application = await Application.findById(id).catch(() => {
+        res.status(404).json({
+            message: `Application ${id} not found`,
+        });
+    });
+
+    if (application) {
+        application.name = body.name;
+        application.studentId = body.studentId;
+        application.streetAddress = body.streetAddress;
+        application.city = body.city;
+        application.state = body.state;
+        application.zipCode = body.zipCode;
+        application.citizen = body.citizen;
+        application.previousExperience = body.previousExperience;
+        application.militaryExperience = body.militaryExperience;
+        application.contactPhone = body.contactPhone;
+        application.phoneNumber = body.phoneNumber;
+
+        application.save()
+            .then(() => {
+                return res.status(200).json({
+                    data: application,
+                });
+            })
+            .catch(error => {
+                return res.status(400).json({
+                    error,
+                    message: "There was an error updating your application"
+                });
+            })
+    }
 };
 
 deleteApplication = async (req, res) => {
