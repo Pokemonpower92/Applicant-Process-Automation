3d40454ec84130acdc6299743e3bf5350fb44cb8
jcarmac3 <pokemonpower92@gmail.com>
2022-03-03 09:58:29 -0500

    Form Styling done

--- app/client/src/pages/ApplicationFormPage.js
+++ app/client/src/pages/ApplicationFormPage.js
@@ -6,7 +6,9 @@ class ApplicationFormPage extends Component {
 		super(props);
 		this.state = {
 			submitted: false,
-			name: "",
+			firstName: "",
+			middleName: "",
+			lastName: "",
 			id: "",
 			address: "",
 			city: "",
@@ -15,13 +17,17 @@ class ApplicationFormPage extends Component {
 			phone: "",
 			email: "",
 			citizen: false,
-			previousExperience: "",
-			militaryExperience: "",
+			previousExperience: false,
+			previousExperienceDetails: "",
+			militaryExperience: false,
+			militaryExperienceDetails: "",
 			contactAgreement: false,
 		};
 
 		this.handleSubmitClick = this.handleSubmitClick.bind(this);
-		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+		this.handleMiddleNameChange = this.handleMiddleNameChange.bind(this);
+		this.handleLastNameChange = this.handleLastNameChange.bind(this);
 		this.handleIdChange = this.handleIdChange.bind(this);
 		this.handleAddressChange = this.handleAddressChange.bind(this);
 		this.handleCityChange = this.handleCityChange.bind(this);
@@ -32,8 +38,12 @@ class ApplicationFormPage extends Component {
 		this.handleCitizenChange = this.handleCitizenChange.bind(this);
 		this.handlePreviousExperienceChange =
 			this.handlePreviousExperienceChange.bind(this);
+		this.handlePreviousExperienceDetailsChange =
+			this.handlePreviousExperienceDetailsChange.bind(this);
 		this.handleMilitaryExperienceChange =
 			this.handleMilitaryExperienceChange.bind(this);
+		this.handleMilitaryExperienceDetailsChange =
+			this.handleMilitaryExperienceDetailsChange.bind(this);
 		this.handleContactAggreementChange =
 			this.handleContactAggreementChange.bind(this);
 	}
@@ -49,7 +59,19 @@ class ApplicationFormPage extends Component {
 		history.push(`/ApplicationForm`);
 	};
 
-	handleNameChange = (e) => {
+	handleFirstNameChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleMiddleNameChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleLastNameChange = (e) => {
 		this.setState({
 			[e.target.name]: e.target.value,
 		});
@@ -105,12 +127,26 @@ class ApplicationFormPage extends Component {
 	};
 
 	handlePreviousExperienceChange = (e) => {
+		const eValue = e.currentTarget.value === "true" ? true : false;
+		this.setState({
+			[e.target.name]: eValue,
+		});
+	};
+
+	handlePreviousExperienceDetailsChange = (e) => {
 		this.setState({
 			[e.target.name]: e.target.value,
 		});
 	};
 
 	handleMilitaryExperienceChange = (e) => {
+		const eValue = e.currentTarget.value === "true" ? true : false;
+		this.setState({
+			[e.target.name]: eValue,
+		});
+	};
+
+	handleMilitaryExperienceDetailsChange = (e) => {
 		this.setState({
 			[e.target.name]: e.target.value,
 		});
@@ -138,153 +174,245 @@ class ApplicationFormPage extends Component {
 					<h2>Student Information</h2>
 				</div>
 				<form className="ApplicationFormPage-unsubmitted-form">
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>Name: </label>
-						<input
-							type="text"
-							name="name"
-							value={this.state.name}
-							onChange={this.handleNameChange}
-							className="ApplicationFormPage-unsubmitted-form-firstName"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>UTK Student ID: </label>
-						<input
-							type="text"
-							name="id"
-							value={this.state.id}
-							onChange={this.handleIdChange}
-							className="ApplicationFormPage-unsubmitted-form-id"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>Street Address: </label>
-						<input
-							type="text"
-							name="address"
-							value={this.state.address}
-							onChange={this.handleAddressChange}
-							className="ApplicationFormPage-unsubmitted-form-address"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>City: </label>
-						<input
-							type="text"
-							name="city"
-							value={this.state.city}
-							onChange={this.handleCityChange}
-							className="ApplicationFormPage-unsubmitted-form-city"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>State: </label>
-						<input
-							type="text"
-							name="state"
-							value={this.state.state}
-							onChange={this.handleStateChange}
-							className="ApplicationFormPage-unsubmitted-form-state"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>Zip: </label>
-						<input
-							type="text"
-							name="zip"
-							value={this.state.zip}
-							onChange={this.handleZipChange}
-							className="ApplicationFormPage-unsubmitted-form-zip"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>Phone Number: </label>
-						<input
-							type="text"
-							name="phone"
-							value={this.state.phone}
-							onChange={this.handlePhoneChange}
-							className="ApplicationFormPage-unsubmitted-form-phone"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textInput">
-						<label>Email: </label>
-						<input
-							type="text"
-							name="email"
-							value={this.state.email}
-							onChange={this.handleEmailChange}
-							className="ApplicationFormPage-unsubmitted-form-email"
-						></input>
-					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-radioInput">
-						<label>US Citizen: </label>
-						<div className="ApplicationFormPage-unsubmitted-form-radioGroup">
-							<input
-								type="radio"
-								name="citizen"
-								value={true}
-								checked={this.state.citizen === true}
-								onChange={this.handleCitizenChange}
-								className="ApplicationFormPage-unsubmitted-form-citizen"
-							/>
-							Yes
-							<input
-								type="radio"
-								name="citizen"
-								value={false}
-								checked={this.state.citizen === false}
-								onChange={this.handleCitizenChange}
-								className="ApplicationFormPage-unsubmitted-form-citizen"
-							/>
-							No
+					<div className="ApplicationFormPage-unsubmitted-form-name">
+						<div className="ApplicationFormPage-unsubmitted-form-inputTitle">
+							<p>Identification</p>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-name-fields">
+							<div className="ApplicationFormPage-unsubmitted-form-nameTextInput">
+								<input
+									type="text"
+									name="name"
+									value={this.state.name}
+									onChange={this.handleFirstNameChange}
+									className="ApplicationFormPage-unsubmitted-form-nameFeild"
+								></input>
+								<label>First Name</label>
+							</div>
+							<div className="ApplicationFormPage-unsubmitted-form-nameTextInput">
+								<input
+									type="text"
+									name="name"
+									value={this.state.name}
+									onChange={this.handleMiddleNameChange}
+									className="ApplicationFormPage-unsubmitted-form-nameFeild"
+								></input>
+								<label>Middle Name</label>
+							</div>
+							<div className="ApplicationFormPage-unsubmitted-form-nameTextInput">
+								<input
+									type="text"
+									name="name"
+									value={this.state.name}
+									onChange={this.handleLastNameChange}
+									className="ApplicationFormPage-unsubmitted-form-nameFeild"
+								></input>
+								<label>Last Name</label>
+							</div>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-id">
+							<div className="ApplicationFormPage-unsubmitted-form-textInput">
+								<input
+									type="text"
+									name="id"
+									value={this.state.id}
+									onChange={this.handleIdChange}
+									className="ApplicationFormPage-unsubmitted-form-id"
+								></input>
+								<label>UTK Student ID</label>
+							</div>
 						</div>
 					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-radioInput">
-						<label>Permission to Contact: </label>
-						<div className="ApplicationFormPage-unsubmitted-form-radioGroup">
-							<input
-								type="radio"
-								name="contactAgreement"
-								value={true}
-								checked={this.state.contactAgreement === true}
-								onChange={this.handleContactAggreementChange}
-								className="ApplicationFormPage-unsubmitted-form-citizen"
-							/>
-							Yes
-							<input
-								type="radio"
-								name="contactAgreement"
-								value={false}
-								checked={this.state.contactAgreement === false}
-								onChange={this.handleContactAggreementChange}
-								className="ApplicationFormPage-unsubmitted-form-citizen"
-							/>
-							No
+
+					<div className="ApplicationFormPage-unsubmitted-form-address">
+						<div className="ApplicationFormPage-unsubmitted-form-inputTitle">
+							<p>Address</p>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-address-street">
+							<div className="ApplicationFormPage-unsubmitted-form-streetTextInput">
+								<input
+									type="text"
+									name="address"
+									value={this.state.address}
+									onChange={this.handleAddressChange}
+									className="ApplicationFormPage-unsubmitted-form-street"
+								></input>
+								<label>Street Address</label>
+							</div>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-address-other">
+							<div className="ApplicationFormPage-unsubmitted-form-otherTextInput">
+								<input
+									type="text"
+									name="city"
+									value={this.state.city}
+									onChange={this.handleCityChange}
+									className="ApplicationFormPage-unsubmitted-form-city"
+								></input>
+								<label>City</label>
+							</div>
+							<div className="ApplicationFormPage-unsubmitted-form-otherTextInput">
+								<input
+									type="text"
+									name="state"
+									value={this.state.state}
+									onChange={this.handleStateChange}
+									className="ApplicationFormPage-unsubmitted-form-state"
+								></input>
+								<label>State</label>
+							</div>
+							<div className="ApplicationFormPage-unsubmitted-form-otherTextInput">
+								<input
+									type="text"
+									name="zip"
+									value={this.state.zip}
+									onChange={this.handleZipChange}
+									className="ApplicationFormPage-unsubmitted-form-zip"
+								></input>
+								<label>Zip</label>
+							</div>
 						</div>
 					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textarea">
-						<label>Previous Experience: </label>
-						<textarea
-							type="textarea"
-							name="previousExperience"
-							value={this.state.previousExperience}
-							placeholder="Leave blank if not applicable."
-							onChange={this.handlePreviousExperienceChange}
-							className="ApplicationFormPage-unsubmitted-form-previousExperience"
-						></textarea>
+
+					<div className="ApplicationFormPage-unsubmitted-form-contact">
+						<div className="ApplicationFormPage-unsubmitted-form-inputTitle">
+							<p>Contact Information</p>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-contact-details">
+							<div className="ApplicationFormPage-unsubmitted-form-contactTextInput">
+								<input
+									type="text"
+									name="phone"
+									value={this.state.phone}
+									onChange={this.handlePhoneChange}
+									className="ApplicationFormPage-unsubmitted-form-phone"
+								></input>
+								<label>Phone Number</label>
+							</div>
+							<div className="ApplicationFormPage-unsubmitted-form-contactTextInput">
+								<input
+									type="text"
+									name="email"
+									value={this.state.email}
+									onChange={this.handleEmailChange}
+									className="ApplicationFormPage-unsubmitted-form-email"
+								></input>
+								<label>Email</label>
+							</div>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-radioInput">
+							<label>Permission to Contact: </label>
+							<div className="ApplicationFormPage-unsubmitted-form-radioGroup">
+								<input
+									type="radio"
+									name="contactAgreement"
+									value={true}
+									checked={
+										this.state.contactAgreement === true
+									}
+									onChange={
+										this.handleContactAggreementChange
+									}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								Yes
+								<input
+									type="radio"
+									name="contactAgreement"
+									value={false}
+									checked={
+										this.state.contactAgreement === false
+									}
+									onChange={
+										this.handleContactAggreementChange
+									}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								No
+							</div>
+						</div>
 					</div>
-					<div className="ApplicationFormPage-unsubmitted-form-textarea">
-						<label>Military Experience: </label>
-						<textarea
-							type="radio"
-							name="militaryExperience"
-							value={this.state.militaryExperience}
-							placeholder="Leave blank if not applicable."
-							onChange={this.handleMilitaryExperienceChange}
-							className="ApplicationFormPage-unsubmitted-form-militaryExperience"
-						></textarea>
+					<div className="ApplicationFormPage-unsubmitted-form-experience">
+						<div className="ApplicationFormPage-unsubmitted-form-inputTitle">
+							<p>Experience</p>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-radioInput">
+							<label>Previous Experience: </label>
+							<div className="ApplicationFormPage-unsubmitted-form-radioGroup">
+								<input
+									type="radio"
+									name="citizen"
+									value={true}
+									checked={this.state.citizen === true}
+									onChange={this.handleCitizenChange}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								Yes
+								<input
+									type="radio"
+									name="citizen"
+									value={false}
+									checked={this.state.citizen === false}
+									onChange={this.handleCitizenChange}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								No
+							</div>
+						</div>
+
+						<div className="ApplicationFormPage-unsubmitted-form-textarea">
+							<textarea
+								type="textarea"
+								name="previousExperience"
+								value={this.state.previousExperienceDetails}
+								placeholder="Leave blank if not applicable."
+								onChange={
+									this.handlePreviousExperienceDetailsChange
+								}
+								className="ApplicationFormPage-unsubmitted-form-previousExperience"
+							></textarea>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-textarea">
+							<label>Military Experience: </label>
+							<textarea
+								type="radio"
+								name="militaryExperience"
+								value={this.state.militaryExperienceDetails}
+								placeholder="Leave blank if not applicable."
+								onChange={
+									this.handleMilitaryExperienceDetailsChange
+								}
+								className="ApplicationFormPage-unsubmitted-form-militaryExperience"
+							></textarea>
+						</div>
+					</div>
+					<div className="ApplicationFormPage-unsubmitted-form-citizen">
+						<div className="ApplicationFormPage-unsubmitted-form-inputTitle">
+							<p>Citizenship</p>
+						</div>
+						<div className="ApplicationFormPage-unsubmitted-form-radioInput">
+							<label>US Citizen: </label>
+							<div className="ApplicationFormPage-unsubmitted-form-radioGroup">
+								<input
+									type="radio"
+									name="citizen"
+									value={true}
+									checked={this.state.citizen === true}
+									onChange={this.handleCitizenChange}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								Yes
+								<input
+									type="radio"
+									name="citizen"
+									value={false}
+									checked={this.state.citizen === false}
+									onChange={this.handleCitizenChange}
+									className="ApplicationFormPage-unsubmitted-form-citizen"
+								/>
+								No
+							</div>
+						</div>
 					</div>
 				</form>
 			</div>
